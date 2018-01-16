deaffdd816ffd676a23532cfde3c243358d62693
Faiza Husain and Nanette Girzi <faiza@>
2018-01-16 11:31:38 -0800

    creates new array and displays new array elements as a list

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,13 @@ $(document).ready(function () {
 
     favoriteThings = [($("#color").val()), ($("#animal").val()), ($("#movie").val()), ($("#book").val()), ($("#food").val())];
 
+    var newFavorites = [favoriteThings[1], favoriteThings[2], favoriteThings[3]];
 
-    $("#output").text(favoriteThings)
+    // $("#output").text(favoriteThings)
+    $("#output ul").append("<li>" + newFavorites[0] + "</li>");
+    $("#output ul").append("<li>" + newFavorites[1] + "</li>");
+    $("#output ul").append("<li>" + newFavorites[2] + "</li>");
+    // $("#output ul").append("<li>blah</li>");
     console.log(favoriteThings);
   });
 });
